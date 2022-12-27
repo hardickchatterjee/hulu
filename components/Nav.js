005afad4951de81ba870bdec8d965b98d67a24e5
hardickchatterjee <67796655+hardickchatterjee@users.noreply.github.com>
2022-12-26 23:22:19 -0700

    Update Nav.js

--- components/Nav.js
+++ components/Nav.js
@@ -1,24 +1,32 @@
 import React from 'react'
 import requests from '../utils/requests'
 import {useRouter} from 'next/router'
+import { useState } from 'react'
 
-function Nav() {
+const Nav = () => {
+  const router = useRouter()
+  const [activeItem, setActiveItem] = useState('')
 
-  const router = useRouter(); 
   return (
-    
-    <nav className='relative'>
-      <div className='last:pr-24 flex px-10 sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20'>
-        {Object.entries(requests).map(([key, {title, url}]) =>(
-              <h2 key={key} 
-              onClick={()=>{router.push(`/?genre=${key}`)}}
-              className='cursor-pointer transform hover:text-white active:text-red-500 transition hover:scale-125'>{title}</h2>
+    <nav className="relative">
+      <div className="flex px-10 space-x-10 overflow-x-scroll text-2xl sm:px-20 whitespace-nowrap sm:space-x-20">
+        {Object.entries(requests).map(([key, { title, url }]) => ( 
+          <h2
+            key={key}
+            onClick={() =>
+              router.push(`/?genre=${key}`) && setActiveItem(title)
+            }
+            className={`transition duration-100 transform cursor-pointer last:pr-24 hover:scale-125 hover:text-green-500 active:text-green-500 ${
+              activeItem == title && 'text-green-500' 
+            }`}
+          >
+            {title}
+          </h2>
         ))}
-        </div>
-        <div className='absolute top-0 right-0 bg-gradient-to-l'>
-          
-        </div>
-        <br/><br/>
+      </div>
+      {/* Fade Effect */}
+      <div className="absolute top-0 left-0 bg-gradient-to-r to-transparent from-[#06202A] h-10 w-1/12" />
+      <div className="absolute top-0 right-0 bg-gradient-to-l to-transparent from-[#06202A] h-10 w-1/12" />
     </nav>
   )
 }
